0df1f2c301cf26f5622e460a34bb6ecd7f1af13d
Marek Kaluba <kalmar@amu.edu.pl>
2023-12-12 19:00:11 +0100

    simplify AlgebraElement

--- src/algebra_elts.jl
+++ src/algebra_elts.jl
@@ -5,40 +5,14 @@ function Base.:(==)(X::AlgebraElement, Y::AlgebraElement)
     return coeffs(X) == coeffs(Y)
 end
 
-Base.getindex(a::AlgebraElement, i::Integer) = coeffs(a)[i]
-Base.getindex(a::AlgebraElement{<:StarAlgebra{O,T}}, x::T) where {O,T} =
-    (b = basis(parent(a)); a[b[x]])
-
+Base.getindex(a::AlgebraElement, x) = coeffs(a)[x]
+Base.setindex!(a::AlgebraElement, v, idx) = a.coeffs[idx] = v
 # call overload:
 (a::AlgebraElement)(x) = a[x]
 
-Base.setindex!(a::AlgebraElement, v, i::Integer) = a.coeffs[i] = v
-
-function Base.setindex!(
-    a::AlgebraElement{<:StarAlgebra{O,T}},
-    v,
-    t::T,
-) where {O,T}
-    b = basis(parent(a))
-    return a[b[t]] = v
-end
-
 # AlgebraElement specific functions
 
-supp_ind(a::AlgebraElement) = findall(!iszero, coeffs(a))
-supp_ind(a::AlgebraElement{A,T,<:SparseVector}) where {A,T} =
-    (dropzeros!(coeffs(a)); SparseArrays.nonzeroinds(coeffs(a)))
-supp(a::AlgebraElement) = (b = basis(parent(a)); [b[i] for i in supp_ind(a)])
-
-function star(A::StarAlgebra, i::Integer)
-    @assert i > 0
-    if i < max(size(A.mstructure)...)
-        return _get(A.mstructure, -signed(i))
-    else
-        b = basis(A)
-        return b[star(b[i])]
-    end
-end
+supp(a::AlgebraElement) = (b = basis(parent(a)); [b[i] for i in keys(coeffs(a))])
 
 function star(X::AlgebraElement)
     res = star(basis(parent(X)), coeffs(X))
@@ -56,9 +30,13 @@ LinearAlgebra.dot(a::AlgebraElement, v::AbstractVector) =
 LinearAlgebra.dot(v::AbstractVector, a::AlgebraElement) = LinearAlgebra.dot(a, v)
 
 Base.copy(a::AlgebraElement) = AlgebraElement(copy(coeffs(a)), parent(a))
-function Base.deepcopy_internal(a::AlgebraElement, stackdict::IdDict)
-    haskey(stackdict, a) && return stackdict[a]
-    return AlgebraElement(Base.deepcopy_internal(coeffs(a), stackdict), parent(a))
+
+function Base.deepcopy_internal(a::AlgebraElement, IdDict::IdDict)
+    if !haskey(IdDict, a)
+        IdDict[a] = AlgebraElement(Base.deepcopy_internal(coeffs(a), IdDict), parent(a))
+    end
+    return IdDict[a]
+end
 
 function fmac!(
     res::AlgebraElement{A,T,<:SparseCoefficients},
