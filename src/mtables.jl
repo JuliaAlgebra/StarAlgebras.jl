b77e698a8be221261216015f358d4ae3c1bfc87a
Marek Kaluba <kalmar@amu.edu.pl>
2023-12-31 22:39:51 +0100

    reimplement MTables

--- src/mtables.jl
+++ src/mtables.jl
@@ -2,66 +2,81 @@
     MTable{T, I} <: MultiplicativeStructure{T}
 Multiplicative table, stored explicitly as an AbstractMatrix{I}.
 
-Requires integer keys in `basis(mt)`.
-
 !!! note
     Accessing `mt[i,j]` with negative indices returns the product of
     `star`ed basis elements, i.e.
     ```julia
-    b = basis(mt)
     mt[-i, j] == b[star(b[i])*b[j]]
     ```
 """
-struct MTable{
-    T,
-    I,
-    B<:AbstractBasis{T,I},
-    M<:AbstractMatrix{I},
-} <: MultiplicativeStructure{I}
-    basis::B
+struct MTable{T,I,V<:AbstractVector,M<:AbstractMatrix,Ms} <:
+       MultiplicativeStructure
+    elts::V
+    relts::Dict{T,I}
+    starof::Vector{I}
     table::M
+    mstr::Ms
 end
 
-function MTable(basis::AbstractBasis{T,I}; size::Tuple{Integer,Integer}) where {T,I<:Integer}
-    @assert Base.haslength(basis)
-    return MTable(basis, zeros(I, size))
+function MTable(
+    elts::AbstractVector,
+    mstr::MultiplicativeStructure,
+    dims::NTuple{2,I},
+) where {I<:Integer}
+    Base.require_one_based_indexing(elts)
+    @assert length(elts) ≤ first(dims)
+    @assert length(elts) ≤ last(dims)
+
+    relts = Dict(b => I(idx) for (idx, b) in pairs(elts))
+    starof = [relts[star(x)] for x in elts]
+    T = typeof(mstr(first(elts), first(elts)))
+    table = Matrix{T}(undef, dims)
+
+    return MTable(elts, relts, starof, table, mstr)
 end
 
-basis(mt::MTable) = mt.basis
+Base.@propagate_inbounds function __absindex(mt::MTable, i::Integer)
+    return ifelse(i > 0, i, oftype(i, mt.starof[abs(i)]))
+end
+
 Base.size(mt::MTable) = size(mt.table)
+Base.haskey(mt::MTable, x) = haskey(mt.relts, x)
+Base.getindex(mt::MTable{T}, x::T) where {T} = mt.relts[x]
+Base.@propagate_inbounds Base.getindex(mt::MTable, i::Integer) =
+    mt.elts[__absindex(mt, i)]
 
-Base.@propagate_inbounds __iscomputed(mt::MTable, i, j) = !iszero(mt.table[i, j])
-Base.@propagate_inbounds function Base.getindex(mt::MTable, i::Integer, j::Integer)
-    b = basis(mt)
-    i = ifelse(i ≥ 0, i, oftype(i, b[b[i]]))
-    j = ifelse(j ≥ 0, j, oftype(j, b[b[i]]))
-    @boundscheck checkbounds(mt.table, i, j)
+Base.@propagate_inbounds __iscomputed(mt::MTable, i, j) =
+    isassigned(mt.table, i, j) && !iszero(mt.table[i, j])
 
-    @inbounds if !__iscomputed(mt, i, j)
-        __compute_product!(mt, i, j)
+Base.@propagate_inbounds function (mt::MTable)(i::Integer, j::Integer)
+    @boundscheck checkbounds(mt.table, abs(i), abs(j))
+    @inbounds begin
+        i = __absindex(mt, i)
+        j = __absindex(mt, j)
+
+        if !__iscomputed(mt, i, j)
+            complete!(mt, i, j)
+        end
+        return mt.table[i, j]
     end
-    @inbounds return mt.table[i, j]
+end
+Base.@propagate_inbounds function (mt::MTable{T})(x::T, y::T) where {T}
+    i, j = mt[x], mt[y]
+    return @inbounds mt(i, j)
 end
 
-Base.@propagate_inbounds function __compute_product!(mt::MTable, i::Integer, j::Integer)
-    b = basis(mt)
-    g, h = b[i], b[j]
-    gh = g * h
-    gh in b || throw(ProductNotWellDefined(i, j, "$g · $h = $gh"))
-    mt.table[i, j] = b[gh]
+Base.@propagate_inbounds function complete!(mt::MTable, i::Integer, j::Integer)
+    g, h = mt[i], mt[j]
+    gh_cfs = mt.mstr(g, h)
+    mt.table[i, j] = gh_cfs
     return mt
 end
 
-function complete!(table::AbstractMatrix, basis::AbstractBasis{V,K}) where {V,K<:Integer}
-    Base.require_one_based_indexing(table)
-    Threads.@threads for j in axes(table, 2)
-        y = basis[j]
-        for i in axes(table, 1)
-            xy = basis[i] * y
-            table[i, j] = basis[xy]
+function complete!(mt::MTable)
+    Threads.@threads for j in axes(mt.table, 2)
+        for i in axes(mt.table, 1)
+            @inbounds complete!(mt, i, j)
         end
     end
-    return table
+    return mt
 end
-
-complete!(mt::MTable) = complete!(mt.table, basis(mt))
