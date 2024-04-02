cdea07cab36769cc0d3d49cbebdf83ab08dd01f4
Marek Kaluba <kalmar@amu.edu.pl>
2024-04-02 03:37:18 +0200

    [tests] update constructors and sohs in group algebra

--- test/constructors.jl
+++ test/constructors.jl
@@ -1,78 +1,84 @@
 @testset "Algebra and Elements" begin
-    A = [:a, :b, :c]
-    b = StarAlgebras.Basis{UInt8}(words(A, radius=2))
-    l = length(b)
-
-    RG = StarAlgebra(one(first(b)), b, (4, 4))
-
-    a = rand(l)
-
-    @test AlgebraElement(a, RG) isa AlgebraElement
-    @test all(RG(g) isa AlgebraElement{typeof(RG)} for g in b)
+    alph = [:a, :b, :c]
+    A★ = FreeWords(alph)
+    B = SA.DiracBasis{UInt16}(A★)
+    RG = StarAlgebra(A★, B)
 
     @test typeof(zero(RG)) == typeof(RG(0))
     @test typeof(one(RG)) == typeof(RG(1))
 
-    @test_throws AssertionError AlgebraElement([1, 2, 3], RG)
-    @test AlgebraElement([1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], RG) isa AlgebraElement
+    @test one(RG) == RG(1)
+    @test RG(1) == one(RG(0))
 
-    x = AlgebraElement([1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], RG)
-    @test AlgebraElement{Float64}(x) isa AlgebraElement
-    y = AlgebraElement{Float64}(x)
-    @test parent(x) === parent(y)
-    @test x == y
+    @test zero(RG) == RG(0)
+    @test RG(0) == zero(RG(1))
 
-    p = Word(A, [2, 3])
+    x = let A = alph, n = 7
+        elts = [Word(A, rand(1:length(A), rand(0:7))) for _ in 1:n]
+        vals = rand(-5:5, n)
+        coeffs = SA.SparseCoefficients(elts, vals)
+        MA.operate!(SA.canonical, coeffs)
+    end
+
+    @test AlgebraElement(x, RG) isa AlgebraElement
+
+    X = AlgebraElement(x, RG)
+
+    @test AlgebraElement{Float64}(X) isa AlgebraElement
+    Y = AlgebraElement{Float64}(X)
+    @test parent(X) === parent(Y)
+    @test X == Y
+
+    p = Word(alph, [2, 3])
     a = RG(p)
-    @test coeffs(a)[b[p]] == 1
-    @test coeffs(a) isa SparseVector
-    @test all(coeffs(a)[i] == 0 for i in 1:length(b) if i ≠ b[p])
+    @test coeffs(a)[B[p]] == 1
+    @test coeffs(a) isa SA.SparseCoefficients
+    @test length(collect(SA.nonzero_pairs(coeffs(a)))) == 1
     @test a(p) == 1
-    @test all(a(g) == 0 for g in b if g != p)
+
+    let b = collect(Iterators.take(B, 121))
+        @test all(coeffs(a)[x] == 0 for x in b if x ≠ p)
+        @test all(a(g) == 0 for g in b if g != p)
+    end
 
     @test sprint(show, zero(RG)) == "0·(id)"
     @test sprint(show, one(RG)) == "1·(id)"
     @test isone(one(a))
-    @test !isone(x)
+    @test !isone(X)
     @test iszero(zero(a))
     @test sprint(show, a) == "1·b·c"
     @test sprint(show, -a) == "-1·b·c"
 
     @test hash(a) == hash(one(RG) + RG(p) - one(RG))
 
-    z = zeros(l)
-    z[b[p]] = 1
-    @test AlgebraElement(z, RG) == a
+    @test SA.supp(a) == [p]
+    @test collect(SA.nonzero_pairs(coeffs(a))) == [(p => 1)]
 
-    @test StarAlgebras.supp(a) == [p]
-    @test StarAlgebras.supp_ind(a) == [b[p]]
-
-    s = one(first(b))
+    s = one(first(B))
     @test a(s) == 0
 
     a[s] = 2
 
-    @test coeffs(a)[b[s]] == 2
-    @test a[b[s]] == 2
+    @test coeffs(a)[s] == 2
     @test a(s) == 2
 
-    dense_a = AlgebraElement(Vector(coeffs(a)), RG)
-    @test a == dense_a
-    @test hash(a) == hash(dense_a)
+    # dense_a = AlgebraElement(Vector(coeffs(a)), RG)
+    # @test a == dense_a
+    # @test hash(a) == hash(dense_a)
 
-    @test StarAlgebras.supp_ind(a) == [b[s], b[p]] == StarAlgebras.supp_ind(dense_a)
-    @test supp(a) == [s, p] == StarAlgebras.supp(dense_a)
+    # @test SA.supp_ind(a) == [b[s], b[p]] == SA.supp_ind(dense_a)
+    # @test supp(a) == [s, p] == SA.supp(dense_a)
 
     aa = a - RG(p)
-    dense_aa = dense_a - RG(p)
-    @test StarAlgebras.supp_ind(aa) == [b[s]] == StarAlgebras.supp_ind(dense_aa)
-    @test supp(aa) == [s] == StarAlgebras.supp(dense_aa)
+    # dense_aa = dense_a - RG(p)
+    # @test SA.supp_ind(aa) == [b[s]] == SA.supp_ind(dense_aa)
+    @test supp(aa) == [s]
 
     @test sprint(show, a) == "2·(id) +1·b·c"
     @test sprint(show, -a) == "-2·(id) -1·b·c"
-    z[b[s]] = 2
-    @test AlgebraElement(z, RG) == a
-    @test sprint(show, AlgebraElement(z, RG)) == "2.0·(id) +1.0·b·c"
+    Z = AlgebraElement{Float64}(a)
+    @test Z == a
+    @test sprint(show, Z) == "2.0·(id) +1.0·b·c"
     @test sprint(show, 2one(RG) - RG(p)) == "2·(id) -1·b·c"
 
     @test LinearAlgebra.norm(a, 1) == 3
@@ -86,16 +92,4 @@
     @test deepcopy(a) !== a
     @test coeffs(deepcopy(a)) !== coeffs(a)
     @test parent(deepcopy(a)) === parent(a)
-
-    @testset "without basis" begin
-        O = one(first(b))
-        RG2 = StarAlgebra(O, RG.mstructure)
-        @test_throws String zero(RG2)
-        @test_throws String one(RG2)
-        @test_throws String RG2(one(O))
-        @test_throws String RG2(-5)
-
-        @test sprint(show, AlgebraElement(rand(-2:2, 6), RG2)) isa String
-        @test sprint(show, AlgebraElement(zeros(Int, 6), RG2)) isa String
-    end
 end
--- test/sum_of_squares.jl
+++ test/sum_of_squares.jl
@@ -1,99 +1,124 @@
 @testset "sum of squares in FreeGroup *-algebra" begin
-      F = Groups.FreeGroup(4)
-      S = [Groups.gens(F); inv.(Groups.gens(F))]
+    F = Groups.FreeGroup(4)
+    S = [Groups.gens(F); inv.(Groups.gens(F))]
 
-      ID = one(F)
-      RADIUS = 3
-      @time E_R, sizes = Groups.wlmetric_ball(S, ID, radius=2 * RADIUS)
-      @test sizes == [9, 65, 457, 3201, 22409, 156865]
+    ID = one(F)
+    RADIUS = 3
+    E_R, sizes = Groups.wlmetric_ball(S, ID; radius = 2 * RADIUS)
+    @test sizes == [9, 65, 457, 3201, 22409, 156865]
 
-      b = StarAlgebras.Basis{UInt32}(E_R)
+    b = StarAlgebras.DiracBasis{UInt32}(F)
 
-      mstr = StarAlgebras.MTable(b, size=(sizes[RADIUS], sizes[RADIUS]))
+    RG = StarAlgebra(F, b)
 
-      RG = StarAlgebra(F, b, mstr)
+    g, h, k, l = S[1:4]
 
-      g, h, k, l = S[1:4]
+    G = (one(RG) - RG(g))
+    @test G' == one(RG) - RG(inv(g))
+    @test G' * G ==
+          MA.operate_to!(zero(G), *, G', G) ==
+          2one(RG) - RG(g) - RG(g)'
+    @test star(G * G) == G' * G'
 
-      G = (one(RG) - RG(g))
-      @test G' == one(RG) - RG(inv(g))
-      @test G' * G == MA.operate_to!(zero(G), *, G', G) == 2one(RG) - RG(g) - RG(g)'
-      @test star(G * G) == G' * G'
+    @testset "Sums of hermitian squares" begin
+        𝕀 = one(RG)
 
-      @testset "Sums of hermitian squares" begin
-            𝕀 = one(RG)
+        G = (𝕀 - RG(g))
+        H = (𝕀 - RG(h))
+        K = (𝕀 - RG(k))
+        L = (𝕀 - RG(l))
+        GH = (𝕀 - RG(g * h))
+        KL = (𝕀 - RG(k * l))
 
-            G = (𝕀 - RG(g))
-            H = (𝕀 - RG(h))
-            K = (𝕀 - RG(k))
-            L = (𝕀 - RG(l))
-            GH = (𝕀 - RG(g * h))
-            KL = (𝕀 - RG(k * l))
+        X = (2𝕀 - star(RG(g)) - RG(h))
+        Y = (2𝕀 - star(RG(g * h)) - RG(k))
 
-            X = (2𝕀 - star(RG(g)) - RG(h))
-            Y = (2𝕀 - star(RG(g * h)) - RG(k))
+        @test -(2𝕀 - RG(g * h) - RG(g * h)') + 2G' * G + 2H' * H == X' * X
+        @test (2𝕀 - RG(g * h) - RG(g * h)') == GH' * GH
+        @test -(2𝕀 - RG(g * h * k) - RG(g * h * k)') + 2GH' * GH + 2K' * K ==
+              Y' * Y
+        @test -(2𝕀 - RG(g * h * k) - RG(g * h * k)') +
+              2(GH' * GH - 2G' * G - 2H' * H) +
+              4G' * G +
+              4H' * H +
+              2K' * K == Y' * Y
 
-            @test -(2𝕀 - RG(g * h) - RG(g * h)') + 2G' * G + 2H' * H == X' * X
-            @test (2𝕀 - RG(g * h) - RG(g * h)') == GH' * GH
-            @test -(2𝕀 - RG(g * h * k) - RG(g * h * k)') + 2GH' * GH + 2K' * K == Y' * Y
-            @test -(2𝕀 - RG(g * h * k) - RG(g * h * k)') +
-                  2(GH' * GH - 2G' * G - 2H' * H) +
-                  4G' * G +
-                  4H' * H +
-                  2K' * K == Y' * Y
+        @test GH' * GH - 2G' * G - 2H' * H == -X' * X
+        @test -(2𝕀 - RG(g * h * k) - RG(g * h * k)') +
+              4G' * G +
+              4H' * H +
+              2K' * K == 2X' * X + Y' * Y
 
-            @test GH' * GH - 2G' * G - 2H' * H == -X' * X
-            @test -(2𝕀 - RG(g * h * k) - RG(g * h * k)') + 4G' * G + 4H' * H + 2K' * K == 2X' * X + Y' * Y
+        @test GH' * GH ==
+              2G' * G + 2H' * H - (2𝕀 - RG(g)' - RG(h))' * (2𝕀 - RG(g)' - RG(h))
+        @test KL' * KL ==
+              2K' * K + 2L' * L - (2𝕀 - RG(k)' - RG(l))' * (2𝕀 - RG(k)' - RG(l))
 
-            @test GH' * GH == 2G' * G + 2H' * H - (2𝕀 - RG(g)' - RG(h))' * (2𝕀 - RG(g)' - RG(h))
-            @test KL' * KL == 2K' * K + 2L' * L - (2𝕀 - RG(k)' - RG(l))' * (2𝕀 - RG(k)' - RG(l))
+        @test -(2𝕀 - RG(g * h * k * l)' - RG(g * h * k * l)) +
+              2 * GH' * GH +
+              2 * KL' * KL ==
+              (2𝕀 - RG(g * h)' - RG(k * l))' * (2𝕀 - RG(g * h)' - RG(k * l))
 
-            @test -(2𝕀 - RG(g * h * k * l)' - RG(g * h * k * l)) + 2 * GH' * GH + 2 * KL' * KL ==
-                  (2𝕀 - RG(g * h)' - RG(k * l))' * (2𝕀 - RG(g * h)' - RG(k * l))
+        @test -(2𝕀 - star(RG(g * h * k * l)) - RG(g * h * k * l)) +
+              2(
+                  2G' * G + 2H' * H -
+                  (2𝕀 - RG(g)' - RG(h))' * (2𝕀 - RG(g)' - RG(h))
+              ) +
+              2(
+                  2K' * K + 2L' * L -
+                  (2𝕀 - RG(k)' - RG(l))' * (2𝕀 - RG(k)' - RG(l))
+              ) ==
+              (2𝕀 - RG(g * h)' - RG(k * l))' * (2𝕀 - RG(g * h)' - RG(k * l))
 
-            @test -(2𝕀 - star(RG(g * h * k * l)) - RG(g * h * k * l)) +
-                  2(2G' * G + 2H' * H - (2𝕀 - RG(g)' - RG(h))' * (2𝕀 - RG(g)' - RG(h))) +
-                  2(2K' * K + 2L' * L - (2𝕀 - RG(k)' - RG(l))' * (2𝕀 - RG(k)' - RG(l))) ==
-                  (2𝕀 - RG(g * h)' - RG(k * l))' * (2𝕀 - RG(g * h)' - RG(k * l))
+        @test -(2𝕀 - star(RG(g * h * k * l)) - RG(g * h * k * l)) +
+              2(2G' * G + 2H' * H) +
+              2(2K' * K + 2L' * L) ==
+              (2𝕀 - RG(g * h)' - RG(k * l))' * (2𝕀 - RG(g * h)' - RG(k * l)) +
+              2(2𝕀 - RG(g)' - RG(h))' * (2𝕀 - RG(g)' - RG(h)) +
+              2(2𝕀 - RG(k)' - RG(l))' * (2𝕀 - RG(k)' - RG(l))
 
-            @test -(2𝕀 - star(RG(g * h * k * l)) - RG(g * h * k * l)) +
-                  2(2G' * G + 2H' * H) +
-                  2(2K' * K + 2L' * L) ==
-                  (2𝕀 - RG(g * h)' - RG(k * l))' * (2𝕀 - RG(g * h)' - RG(k * l)) +
-                  2(2𝕀 - RG(g)' - RG(h))' * (2𝕀 - RG(g)' - RG(h)) +
-                  2(2𝕀 - RG(k)' - RG(l))' * (2𝕀 - RG(k)' - RG(l))
+        @test -(2𝕀 - RG(g * h * k * l)' - RG(g * h * k * l)) +
+              2(2𝕀 - RG(g * h * k)' - RG(g * h * k)) +
+              2L' * L ==
+              (2𝕀 - RG(g * h * k)' - RG(l))' * (2𝕀 - RG(g * h * k)' - RG(l))
 
-            @test -(2𝕀 - RG(g * h * k * l)' - RG(g * h * k * l)) +
-                  2(2𝕀 - RG(g * h * k)' - RG(g * h * k)) + 2L' * L ==
-                  (2𝕀 - RG(g * h * k)' - RG(l))' * (2𝕀 - RG(g * h * k)' - RG(l))
+        @test 2𝕀 - RG(g * h * k)' - RG(g * h * k) ==
+              2GH' * GH + 2K' * K -
+              (2𝕀 - star(RG(g * h)) - RG(k))' * (2𝕀 - star(RG(g * h)) - RG(k))
 
-            @test 2𝕀 - RG(g * h * k)' - RG(g * h * k) ==
-                  2GH' * GH + 2K' * K - (2𝕀 - star(RG(g * h)) - RG(k))' * (2𝕀 - star(RG(g * h)) - RG(k))
+        @test -(2𝕀 - RG(g * h * k * l)' - RG(g * h * k * l)) +
+              2(
+                  2GH' * GH + 2K' * K -
+                  (2𝕀 - RG(g * h)' - RG(k))' * (2𝕀 - RG(g * h)' - RG(k))
+              ) +
+              2L' * L ==
+              (2𝕀 - RG(g * h * k)' - RG(l))' * (2𝕀 - RG(g * h * k)' - RG(l))
 
-            @test -(2𝕀 - RG(g * h * k * l)' - RG(g * h * k * l)) +
-                  2(
-                        2GH' * GH + 2K' * K - (2𝕀 - RG(g * h)' - RG(k))' * (2𝕀 - RG(g * h)' - RG(k))
-                  ) + 2L' * L ==
-                  (2𝕀 - RG(g * h * k)' - RG(l))' * (2𝕀 - RG(g * h * k)' - RG(l))
+        @test -(2𝕀 - RG(g * h * k * l)' - RG(g * h * k * l)) +
+              2(2GH' * GH + 2K' * K) +
+              2L' * L ==
+              (2𝕀 - RG(g * h * k)' - RG(l))' * (2𝕀 - RG(g * h * k)' - RG(l)) +
+              2(2𝕀 - RG(g * h)' - RG(k))' * (2𝕀 - RG(g * h)' - RG(k))
 
-            @test -(2𝕀 - RG(g * h * k * l)' - RG(g * h * k * l)) + 2(2GH' * GH + 2K' * K) + 2L' * L ==
-                  (2𝕀 - RG(g * h * k)' - RG(l))' * (2𝕀 - RG(g * h * k)' - RG(l)) +
-                  2(2𝕀 - RG(g * h)' - RG(k))' * (2𝕀 - RG(g * h)' - RG(k))
+        @test -(2𝕀 - RG(g * h * k * l)' - RG(g * h * k * l)) +
+              8G' * G +
+              8H' * H +
+              4K' * K +
+              2L' * L ==
+              (2𝕀 - RG(g * h * k)' - RG(l))' * (2𝕀 - RG(g * h * k)' - RG(l)) +
+              2(2𝕀 - RG(g * h)' - RG(k))' * (2𝕀 - RG(g * h)' - RG(k)) +
+              4(2𝕀 - RG(g)' - RG(h))' * (2𝕀 - RG(g)' - RG(h))
 
-            @test -(2𝕀 - RG(g * h * k * l)' - RG(g * h * k * l)) +
-                  8G' * G + 8H' * H + 4K' * K + 2L' * L ==
-                  (2𝕀 - RG(g * h * k)' - RG(l))' * (2𝕀 - RG(g * h * k)' - RG(l)) +
-                  2(2𝕀 - RG(g * h)' - RG(k))' * (2𝕀 - RG(g * h)' - RG(k)) +
-                  4(2𝕀 - RG(g)' - RG(h))' * (2𝕀 - RG(g)' - RG(h))
+        @test -(2𝕀 - RG(g * h * k * l)' - RG(g * h * k * l)) +
+              2GH' * GH +
+              2KL' * KL ==
+              (2𝕀 - RG(g * h)' - RG(k * l))' * (2𝕀 - RG(g * h)' - RG(k * l))
 
-            @test -(2𝕀 - RG(g * h * k * l)' - RG(g * h * k * l)) + 2GH' * GH + 2KL' * KL ==
-                  (2𝕀 - RG(g * h)' - RG(k * l))' * (2𝕀 - RG(g * h)' - RG(k * l))
-
-            @test -(2𝕀 - RG(g * h * k * l)' - RG(g * h * k * l)) +
-                  2(2G' * G + 2H' * H) +
-                  2(2K' * K + 2L' * L) ==
-                  (2𝕀 - RG(g * h)' - RG(k * l))' * (2𝕀 - RG(g * h)' - RG(k * l)) +
-                  2(2𝕀 - RG(k)' - RG(l))' * (2𝕀 - RG(k)' - RG(l)) +
-                  2(2𝕀 - RG(g)' - RG(h))' * (2𝕀 - RG(g)' - RG(h))
-      end
+        @test -(2𝕀 - RG(g * h * k * l)' - RG(g * h * k * l)) +
+              2(2G' * G + 2H' * H) +
+              2(2K' * K + 2L' * L) ==
+              (2𝕀 - RG(g * h)' - RG(k * l))' * (2𝕀 - RG(g * h)' - RG(k * l)) +
+              2(2𝕀 - RG(k)' - RG(l))' * (2𝕀 - RG(k)' - RG(l)) +
+              2(2𝕀 - RG(g)' - RG(h))' * (2𝕀 - RG(g)' - RG(h))
+    end
 end
