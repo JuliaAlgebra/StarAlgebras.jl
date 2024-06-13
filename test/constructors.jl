ba9429be1deb2bda077f85b1559073a761ce06ef
Benoît Legat <benoit.legat@gmail.com>
2024-06-13 15:11:08 +0200

    Allow coeffs not defining zero (#45)
    * Allow coeffs not defining zero
    
    * Add test
    
    * Add comment

--- test/constructors.jl
+++ test/constructors.jl
@@ -109,12 +109,15 @@ end
     @test parent(deepcopy(a)) === parent(a)
 
     latex = CustomLaTeXPrint(" \$\$ \\[\\(α_β∀ \\) \\]\t  \$\$")
-    @test sprint((io, x) -> show(io, "text/latex", x),
-        SA.AlgebraElement(SA.SparseCoefficients([p], [latex]), RG)) ==
+    a = SA.AlgebraElement(SA.SparseCoefficients([p], [latex]), RG)
+    # Tests that `getindex` works even if `zero(typeof(latex))` is not defined
+    @test SA.coeffs(a)[p] == latex
+    @test sprint((io, x) -> show(io, "text/latex", x), a) ==
           "\$\$ (α_β∀) \\cdot b·c \$\$"
     # Test that the check for `\\)` handles unicode well
     latex = CustomLaTeXPrint("\\(β∀")
     @test sprint((io, x) -> show(io, "text/latex", x),
         SA.AlgebraElement(SA.SparseCoefficients([p], [latex]), RG)) ==
           "\$\$ (\\(β∀) \\cdot b·c \$\$"
+    
 end
