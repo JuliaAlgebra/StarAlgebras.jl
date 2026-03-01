d9cbb13cc14d72e0638e8c8fdb6f5f7576b5f940
Benoît Legat <benoit.legat@gmail.com>
2026-03-01 12:42:36 +0100

    Apply suggestion from @blegat

--- test/perm_grp_algebra.jl
+++ test/perm_grp_algebra.jl
@@ -89,7 +89,7 @@ import StarAlgebras as SA
 
         fRG = SA.StarAlgebra(G, SA.MTable(fb, (m, m)))
 
-        @testset "seed=$seed" for seed in 0:100
+        @testset "seed=$seed" for seed in 0:20
             Random.seed!(seed)
             rcfs = SA.SparseCoefficients(rand(G, 10), rand(-2:2, 10))
             r = SA.AlgebraElement(rcfs, RG)
