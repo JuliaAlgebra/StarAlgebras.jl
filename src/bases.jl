9d0f36dddd58f2cc7238b49603bb9d975a447133
Benoît Legat <benoit.legat@gmail.com>
2026-03-01 10:05:43 +0100

    Fix coeffs += vs =

--- src/bases.jl
+++ src/bases.jl
@@ -85,7 +85,9 @@ function coeffs!(res, cfs, source::AbstractBasis, target::AbstractBasis)
     MA.operate!(zero, res)
     for (k, v) in nonzero_pairs(cfs)
         x = source[k]
-        res[target[x]] = v
+        i = target[x]
+        cur = res[i]
+        res[i] = iszero(cur) ? v : cur + v
     end
     return res
 end
