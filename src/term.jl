558f2f15ad9ea3174c57a130a4bae30901c8ac56
Benoît Legat <benoit.legat@gmail.com>
2026-04-12 19:53:33 +0200

    Add operate! methods

--- src/term.jl
+++ src/term.jl
@@ -118,6 +118,25 @@ function LinearAlgebra.dot(t::Term, x)
     return star(t) * x
 end
 
-# MA.operate_to!(::Term, ::typeof(*), ...), MA.operate!(*, ...) and
-# MA.operate!(one, ...) are defined by downstream packages (e.g.,
-# MultivariatePolynomials) which provide polynomial-specific implementations.
+function MA.operate_to!(
+    t::Term,
+    ::typeof(*),
+    t1::Term,
+    t2::Term,
+)
+    MA.operate_to!(t.coefficient, *, coefficient(t1), coefficient(t2))
+    MA.operate_to!(t.basis_element, *, basis_element(t1), basis_element(t2))
+    return t
+end
+
+function MA.operate!(::typeof(*), t1::Term, t2::Term)
+    MA.operate!(*, t1.coefficient, coefficient(t2))
+    MA.operate!(*, t1.basis_element, basis_element(t2))
+    return t1
+end
+
+function MA.operate!(::typeof(one), t::Term)
+    MA.operate!(one, t.coefficient)
+    MA.operate!(one, t.basis_element)
+    return t
+end
